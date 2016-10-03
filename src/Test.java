e5cacba431edcb9bac1b3afb197c4bfa0f9e0afe
sanak <sanak.alex@gmail.com>
2016-10-03 21:19:39 +0300

    Added test class

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Alex on 03.10.2016.
+ */
+public class Test {
+    public static boolean equalProbabilityTest (long[] statistics) {
+        boolean status = false;
+        double n = statistics.length / 256;
+
+        return status;
+    }
+}
